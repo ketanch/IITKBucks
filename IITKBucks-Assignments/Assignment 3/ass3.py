0b9f713b0492fe201986be2c8922e2397777a6ac
ketanch <56212393+ketanch@users.noreply.github.com>
2020-05-14 12:22:47 +0530

    Update ass3.py

--- IITKBucks-Assignments/Assignment 3/ass3.py
+++ IITKBucks-Assignments/Assignment 3/ass3.py
@@ -6,7 +6,10 @@ no_of_inp = int(input("\nENTER NO OF INPUTS: "))
 input_arr = []
 for i in range(no_of_inp):
     print(i+1,".")
-    new_obj = header.Input()
+    transaction_ID = input("Enter transaction ID(sha256): ")
+    arr_index = input("Enter index of output: ")
+    sign = input("Enter valid signature(hex): ")
+    new_obj = header.Input(transaction_ID, arr_index, sign)
     if(not new_obj.isValid()):
         sys.exit()
     input_arr.append(new_obj)
@@ -15,7 +18,9 @@ no_of_out = int(input("\nENTER NO OF OUTPUTS: "))
 output_arr = []
 for i in range(no_of_out):
     print(i+1,".")
-    new_obj = header.Output()
+    coin = input("Enter no of coins: ")
+    path_to_pub = input("Enter path to public key: ")
+    new_obj = header.Output(coin, path_to_pub)
     if(not new_obj.isValid()):
         sys.exit()
     output_arr.append(new_obj)
@@ -28,5 +33,6 @@ try:
     file_out.write(file_data)
     file_out.close()
     print("\nTransaction made successfully")
+    print("Created file: ", file_hash)
 except:
     print("\nSome error occured while making transaction.")
