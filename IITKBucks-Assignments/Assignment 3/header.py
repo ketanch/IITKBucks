dad2be4208c43d72f94576e00d7908ae727af6d0
ketanch <56212393+ketanch@users.noreply.github.com>
2020-05-14 12:23:24 +0530

    Update header.py

--- IITKBucks-Assignments/Assignment 3/header.py
+++ IITKBucks-Assignments/Assignment 3/header.py
@@ -2,22 +2,23 @@ import os.path
 from os import path
 
 class Input:
-    def __init__(self):
-        self.transaction_ID = input("Enter transaction ID(sha256): ")
-        self.arr_index = input("Enter index of output: ")
-        self.sign = input("Enter valid signature(hex): ")
+    def __init__(self, transaction_ID, arr_index, sign):
+        self.transaction_ID = transaction_ID
+        self.arr_index = arr_index
+        self.sign = sign
+        self.len_sign = None
 
     def show(self):
-        print("Transacction ID: ", self.transaction_ID)
-        print("Output Index: ", self.arr_index)
-        print("RSA Signature: ", self.sign)
+        print("\tTransacction ID: ", self.transaction_ID)
+        print("\tIndex: ", self.arr_index)
+        print("\tLength of the signature: ", self.len_sign)
+        print("\tSignature: ", self.sign)
 
     def generateByte(self):
         byte_trans_id = bytes.fromhex(self.transaction_ID)
         byte_arr_index = self.arr_index.to_bytes(4, 'big')
         byte_sign = bytes.fromhex(self.sign)
-        len_sign = len(byte_sign)
-        byte_len_sign = len_sign.to_bytes(4,'big')
+        byte_len_sign = self.len_sign.to_bytes(4,'big')
         byte_final = byte_trans_id+byte_arr_index+byte_len_sign+byte_sign
         return byte_final
 
@@ -29,27 +30,29 @@ class Input:
         elif(not self.arr_index.isdigit()):
             print("\nOutput index not valid.\n")
             return False
-        elif(not all(i in valid_char for i in self.sign)):
+        elif(not (all(i in valid_char for i in self.sign) or len(self.sign)%2==0)):
             print("\nSignature is not valid.\n")
             return False
         self.arr_index = int(self.arr_index)
+        self.len_sign = len(self.sign)//2
         return True
 
 class Output:
-    def __init__(self):
-        self.coin = input("Enter no of coins: ")
-        self.path_to_pub = input("Enter path to public key: ")
-        self.pub_key = ''
+    def __init__(self, coin, path_to_pub):
+        self.coin = coin
+        self.path_to_pub = path_to_pub
+        self.pub_key = None
+        self.key_len = None
 
     def show(self):
-        print("No of coins: ", self.coin)
-        print("Public Key: ", self.pub_key)
+        print("\tNo of coins: ", self.coin)
+        print("\tLength of public key: ", self.key_len)
+        print("\tPublic Key:\n", self.pub_key)
 
     def generateByte(self):
         byte_coin = self.coin.to_bytes(8, 'big')
         byte_pub_key = self.pub_key.encode()
-        key_len = len(byte_pub_key)
-        byte_key_len = key_len.to_bytes(4, 'big')
+        byte_key_len = self.key_len.to_bytes(4, 'big')
         byte_final = byte_coin+byte_key_len+byte_pub_key
         return byte_final
         
@@ -62,6 +65,7 @@ class Output:
             return False
         self.coin = int(self.coin)
         self.pub_key = open(self.path_to_pub,'r').read()
+        self.key_len = len(self.pub_key)
         return True
 
 class Transaction:
@@ -78,5 +82,5 @@ def transactionToByteArray(trans):
         data += i.generateByte()
     return data
 
-def transactionFromByteArray():
+def transactionFromByteArray(trans_byte):
     pass
