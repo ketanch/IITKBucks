7f62e9bf10bc72930f959818b7a09bc3bfaf7847
ketanch <56212393+ketanch@users.noreply.github.com>
2020-06-08 00:03:16 +0530

    Update Input.py

--- Client/modules/Input.py
+++ Client/modules/Input.py
@@ -1,9 +1,12 @@
 class Input:
     def __init__(self, transaction_ID, arr_index, sign, len_sign=None):
         self.transaction_ID = transaction_ID
-        self.arr_index = arr_index
+        self.arr_index = int(arr_index)
         self.sign = sign
-        self.len_sign = len_sign
+        if len_sign == None:
+            self.len_sign = len(self.sign)//2
+        else:
+            self.len_sign = len_sign
 
     def show(self):
         print("\tTransacction ID: ", self.transaction_ID)
@@ -19,5 +22,5 @@ class Input:
         byte_final = byte_trans_id+byte_arr_index+byte_len_sign+byte_sign
         return byte_final
 
-    def generateSign(self, out_hash):
+    def generateSignContent(self, out_hash):
         return bytes.fromhex(self.transaction_ID)+self.arr_index.to_bytes(4, 'big')+out_hash
