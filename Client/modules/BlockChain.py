e22a93728b2b1e99649873d4927373932f1b37ea
ketanch <56212393+ketanch@users.noreply.github.com>
2020-06-14 08:58:12 +0530

    Update BlockChain.py

--- Client/modules/BlockChain.py
+++ Client/modules/BlockChain.py
@@ -1,3 +1,8 @@
 from . import Block
-
 class BlockChain:
+
+    def getPendingTransactions():
+        pending_trans_list = []
+        for transaction in self.pendingTransactions:
+            pending_trans_list.append(transaction.as_json())
+        return pending_trans_list
