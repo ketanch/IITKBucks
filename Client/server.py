7d552411d3b78721c563938e6e60d94569a8c53f
ketanch <56212393+ketanch@users.noreply.github.com>
2020-06-18 12:25:16 +0530

    Update server.py

--- Client/server.py
+++ Client/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, Response
-from models.BlockChain import BlockChain, Block
+from flask import Flask, jsonify, Response, request
+from models.BlockChain import *
 app = Flask(__name__)
 
+peers = []
 blockchain = BlockChain()
 
 @app.route('/getBlock/<int:block>', methods = ["GET"])
@@ -12,3 +13,29 @@ def getBlock(block):
 @app.route('/getPendingTransactions', methods = ["GET"])
 def getPendingTransactions():
     return jsonify(blockchain.getPendingTransactions())
+
+@app.route('/newPeer', methods = ["POST"])
+def new_peer():
+    url_data = request.get_json()
+    peers.append(url_data['url'])
+    return "URL {} was added successfully.".format(url_data['url']), 200
+
+@app.route('/getPeers', methods = ["GET"])
+def get_peer():
+    return jsonify({"peers":peers})
+
+@app.route('/newBlock', methods = ["POST"])
+def new_block():
+    block_data = request.get_data()
+    new_block = Block()
+    new_block.blockFromByte(block_data)
+    blockchain.chain.append(new_block)
+    return "Block successfully added.", 200
+
+@app.route('/newTransaction', methods = ["POST"])
+def new_transaction():
+    json_data = request.get_json()
+    transaction = Transaction()
+    transaction.from_json(json_data)
+    blockchain.pendingTransactions.append(transaction)
+    return "Pending Transactions updated.", 200
