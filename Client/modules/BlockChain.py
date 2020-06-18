bf64d49a6c37761970098300921f67fa610aeeea
ketanch <56212393+ketanch@users.noreply.github.com>
2020-06-18 11:59:06 +0530

    Update BlockChain.py

--- Client/modules/BlockChain.py
+++ Client/modules/BlockChain.py
@@ -11,6 +11,3 @@ class BlockChain:
         for transaction in self.pendingTransactions:
             pending_trans_list.append(transaction.as_json())
         return pending_trans_list
-    
-    def addBlock(self, new_block):
-        self.chain.append(new_block)
