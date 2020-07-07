081a123b63e81dd935b5b38525801f541955d223
ketanch <56212393+ketanch@users.noreply.github.com>
2020-07-07 10:57:37 +0530

    Update miner.py

--- Client/miner.py
+++ Client/miner.py
@@ -1,17 +1,19 @@
 import threading
 from hashlib import sha256
+from modules.Block import Block
 
 class Miner(threading.Thread):
-    def __init__(self):
+    def __init__(self, block):
         super().__init__()
         self.kill = threading.Event()
+        self.block = block
 
-    def run(self, block):
+    def run(self):
         status_code = 404
         for i in range(2**64):
-            block.nonce = i
-            block.timestamp = time.time()
-            if int(sha256(self.getHeader()).hexdigest(),16) <= int(block.target,16) or self.kill.is_set():
+            self.block.nonce = i
+            self.block.timestamp = time.time()
+            if int(sha256(self.getHeader()).hexdigest(),16) <= int(self.block.target,16) or self.kill.is_set():
                 status_code = 200
                 break
         return {"status":status_code}
